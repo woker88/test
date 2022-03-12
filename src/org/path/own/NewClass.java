379bace8f488d090dd855d66a055e2e6c60434c7
woker88 <mail.gomel@mail.ru>
2022-03-12 09:25:59 +0000

    123

--- src/org/path/own/NewClass.java
+++ src/org/path/own/NewClass.java
@@ -3,5 +3,6 @@ package org.path.own;
 public class NewClass {
     public static void main (String[] args){
         System.out.println("test");
+        System.out.println("test3");
     }
 }
---
+++ src/org/path/own/SecondClass.java
@@ -0,0 +1,5 @@
+package org.path.own;
+
+public class SecondClass {
+
+}
